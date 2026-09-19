1ac72245f99805eaad10fb2f986c58f9b6a31467
KrugerEdu <krugeredu07@gmail.com>
2026-09-19 21:15:21 +0000

    projeto1

---
+++ projeto1/safedrive.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define MAX_AMOSTRAS 100
+int main() {
+    int velocidade[MAX_AMOSTRAS][2];
+    int sensores_frontais[MAX_AMOSTRAS][3];
+    int sensores_laterais[MAX_AMOSTRAS][2];
+    int processamento[MAX_AMOSTRAS][2];
+    int status[MAX_AMOSTRAS][3];
+}
